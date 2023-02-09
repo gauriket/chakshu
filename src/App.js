3d08585bac542884976ffc2eff36e9302ecca017
Dhritishah03 <1032211764@mitwpu.edu.in>
2023-02-09 12:44:41 +0530

    Dhriti

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './linechart.js'
 import { Bar } from './linechart.js';
+import { Donut } from './abc.js';
 
 function App() {
   return (
     <div className="container">
       <Bar/>
+      <Donut/>
     </div>
   );
 }
