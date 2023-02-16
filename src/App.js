054c6cb670cdf4ba4d79e8ede2574632c34056f1
Dhritishah03 <1032211764@mitwpu.edu.in>
2023-02-16 15:50:22 +0530

    bhxsbjj

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './linechart.js'
 import { Bar } from './linechart.js';
 import { Toparea } from './toparea.js';
+import { Line } from './line.js';
 import { Donut } from './abc.js';
 import Navbar from './navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -17,11 +18,12 @@ function App() {
       </Router>
        
        <div className="container1">
-        <Bar/><Donut/>
+        <div className='container1-box'><Line/></div>
       </div>
       <div className="container2">
-        <Toparea/>
-      
+        <div className='container2-box1'><Bar/></div> 
+        <div className='container2-box'><Toparea/></div> 
+        <div className='container2-box'><Donut/></div>
       </div>
       <div className="container3"><Example/></div>
       </div>
